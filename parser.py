5178827fa4fcd3ff3ae4c1f5898f5439a0b9a79d
legeorges <georges.labreche@gmail.com>
2014-03-08 01:34:10 -0500

    Implemented rudimentary mongo insertion

--- parser.py
+++ parser.py
@@ -1,10 +1,18 @@
 import urllib
 from xml.dom import minidom
+from pymongo import MongoClient
 
-WP_KOSOVO_REVISIONS_API_URL = 'http://en.wikipedia.org/w/api.php?action=query&titles=Kosovo&prop=revisions&rvlimit=20&format=xml'
+WP_KOSOVO_REVISIONS_API_URL = 'http://en.wikipedia.org/w/api.php?action=query&titles=Kosovo&prop=revisions&rvlimit=500&format=xml'
 
 more_revisions_left = True
 rvcontinue = ''
+
+# connect to default local instance of mongo
+client = MongoClient()
+
+# get database
+revisions_db = client.revisions
+kosovo_collection = revisions_db.kosovo
 
 while more_revisions_left:
 	if not rvcontinue:
@@ -22,7 +30,11 @@ while more_revisions_left:
 		revid = rev.attributes['revid'].value
 		date_str = timestamp[:10]
 		
-		print date_str + ' - ' + revid
+		revision_post = {"revid":revid, "date":date_str, "timestamp":timestamp}
+		
+		post_id = kosovo_collection.insert(revision_post)
+		
+		print revision_post
 
 	# Check if there are more revisions left
 	query_continue = xmldoc.getElementsByTagName('query-continue')
